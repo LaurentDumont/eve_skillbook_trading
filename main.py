4148cfcb4c7c5a9f2f53e579b6e509b97942e9a1
Laurent Dumont <admin@coldnorthadmin.com>
2016-01-24 18:47:10 -0500

    Add Object type for skillbook result & fix result

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ price_list_itamo = []
 crest_url_list = []
 sell_orders_list = []
 pbar = ProgressBar()
-skillbook = []
+skillbook_list = []
 
 class Skillbook:
     profit = 0
@@ -29,10 +29,12 @@ class Skillbook:
         self.price_itamo = price_itamo
 
 def create_Skillbook(profit, name, price_jita, price_itamo):
+
     skillbook = Skillbook(profit, name, price_jita, price_itamo)
     return skillbook
 
 def get_typeID_skillbooks():
+
     print "Reading the file for the typeID"
     with open("eve-skills-typeID.txt") as file:
         typeID = [line.rstrip('\n') for line in file]
@@ -64,41 +66,18 @@ def get_sell_order_crest(typeID):
             print "Failed to connect to the EVE Crest"
     return sell_orders_list
 
-    # For each URL, get the json response
-    # session = FuturesSession()
-    # print "Sending the URL to CREST"
-    # for url in (crest_url_list):
-    #     response = session.get(url)
-    #     sleep(0.5)
-    #     unparsed_sell_orders = response.result()
-    #     sell_orders_list.append(unparsed_sell_orders.json())
-    # for sell_order in enumerate(sell_orders_list):
-    #     print sell_order
-    # return sell_orders_list
-
-    #print unparsed_sell_orders.status_code
-    #print response.result
-    #for json_data in crest_url_list = json_market_data.json()
-    #return parsed_jason_market_data
-
-
 def sort_sell_order_prices(sell_orders_list):
-    #print '\n'.join(str(p) for p in sell_orders_list)
+
     #If the JSON string is invalid, remove from the array and break
     for sell_order in sell_orders_list[:]:
         if sell_order["totalCount_str"] == "0":
-            #print "Removing empty response"
-            #print sell_order
             sell_orders_list.remove(sell_order)
             continue
     #Iterate through the Sell Orders list
-    #print '\n'.join(str(p) for p in sell_orders_list)
     for sell_order in sell_orders_list[:]:
         try:
             skillbook_name = sell_order["items"][0]["type"]["name"]
         except IndexError:
-            #print "Cannot find the name - Probably an empty response"
-            #print sell_order
             sell_orders_list.remove(sell_order)
             continue
         #Iterate through the items to check the location
@@ -106,40 +85,33 @@ def sort_sell_order_prices(sell_orders_list):
 
             if sellOrder["location"]["name"] == "Jita IV - Moon 4 - Caldari Navy Assembly Plant":
                 price_list_jita.append(sellOrder["price"])
-                #price_jita = sellOrder["price"]
-            # else:
-            #     price_jita = 0
 
             if sellOrder["location"]["name"] == "Itamo VI - Moon 6 - Science and Trade Institute School":
                 price_list_itamo.append(sellOrder["price"])
-                #price_itamo = sellOrder["price"]
-            # else:
-            #     price_itamo = 0
-
-        # print "Here is the price list in Jita : %s" %min(price_list_jita)
-        # print "Here is the price list in Itamo : %s"  %min(price_list_itamo)
-        # print "----------------------------"
-        # print "Here is the price list in Jita : %s" %price_jita
-        # print "Here is the price list in Itamo : %s"  %price_itamo
 
         # Calculate price for the item
         try:
             item_profit = min(price_list_jita) - min(price_list_itamo)
         except ValueError:
             continue
+
         comma_item_profit = "ISK {:,.2f}".format(item_profit)
-        #skillbook.append(create_Skillbook(item_profit,skillbook_name,price_jita,price_itamo))
-        print "Profit per skillbook for : %s - %s - Price in Itamo %s" %(skillbook_name,comma_item_profit,min(price_list_itamo))
+        skillbook_list.append(create_Skillbook(comma_item_profit,skillbook_name,min(price_list_jita),min(price_list_itamo)))
+        #print "Profit per skillbook for : %s - %s - Price in Itamo %s" %(skillbook_name,comma_item_profit,min(price_list_itamo))
         price_list_itamo[:] = []
         price_list_jita[:] = []
 
+    return skillbook_list
+
+
 def print_result(skillbook_list):
-    for skillbook in skillbook_list:
+    print skillbook_list.__len__()
+    for skillbook in skillbook_list[:]:
         print skillbook.profit
 
 
 def main():
-    sort_sell_order_prices(get_sell_order_crest(get_typeID_skillbooks()))
+        print_result(sort_sell_order_prices(get_sell_order_crest(get_typeID_skillbooks())))
 
 if __name__ == "__main__":
     main()
