fa386c706609e34c722c250a62a0cef1a973621f
Laurent Dumont <admin@coldnorthadmin.com>
2016-02-16 21:39:51 -0500

    readd url

---
+++ objects/url.py
@@ -0,0 +1,13 @@
+class url:
+   full_url = ""
+   url_location = ""
+
+   def __init__(self, full_url, url_location):
+       self.full_url = full_url
+       self.url_location = url_location
+
+
+def create_url(full_url, url_location):
+
+   url = create_url(full_url, url_location)
+   return url
